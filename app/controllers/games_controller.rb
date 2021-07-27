1df3f04a90eca2fc7a99b487e94c8e4661fa985d
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-07-27 15:32:18 -0400

    It works.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    word = params[:word]
+    @letters = params[:letters].split
+    dictionary = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{word}").read)
+    @result = 0
+    if word.upcase.chars.all? { |x| word.upcase.chars.count(x) <= @letters.count(x) }
+      if dictionary['found']
+        @result = 1 # Congrats
+      else
+        @result = 2 # Not an English word
+      end
+    else
+      @result = 0 # Not in the grid
+    end
+  end
+end
