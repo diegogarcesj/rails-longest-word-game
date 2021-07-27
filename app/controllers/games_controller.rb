e08ec427a88f4d04c06c1fb815b3c174fb80a936
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-07-27 15:50:35 -0400

    Added score with session.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,14 +11,19 @@ class GamesController < ApplicationController
     @letters = params[:letters].split
     dictionary = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{word}").read)
     @result = 0
+    @score = session[:score].to_i || 0
     if word.upcase.chars.all? { |x| word.upcase.chars.count(x) <= @letters.count(x) }
       if dictionary['found']
         @result = 1 # Congrats
+        @score += word.chars.count ** 2;
+        session[:score] = @score
       else
         @result = 2 # Not an English word
       end
     else
       @result = 0 # Not in the grid
     end
+
+
   end
 end
